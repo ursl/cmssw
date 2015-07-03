cf9f70658f33cfa32c24e5ef7f9e5bf7a1aa7dce
Urs Langenegger <urs.langenegger@psi.ch>
2015-07-03 23:54:56 +0200

    Tamas' method of getting the pilot blade geometry

--- CalibTracker/SiPixelConnectivity/test/write_cfg.py
+++ CalibTracker/SiPixelConnectivity/test/write_cfg.py
@@ -4,13 +4,88 @@ process = cms.Process("MapWriter")
 process.load("CondCore.DBCommon.CondDBCommon_cfi")
 process.CondDBCommon.connect = cms.string("sqlite_file:cabling.db")
 
-process.load("Configuration.Geometry.GeometryDB_cff")
-process.load("Geometry.CMSCommonData.cmsExtendedGeometry2015PilotXML_cfi")
-#process.trackerGeometry.applyAlignment = cms.bool(False)
- 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'MCRUN2_74_V7', '')
+print process.GlobalTag.globaltag
+
+process.load('Configuration.Geometry.GeometrySimDB_cff')
+process.load('Configuration.Geometry.GeometryRecoDB_cff')
+
+import CalibTracker.Configuration.Common.PoolDBESSource_cfi
+# ---------------------- PB Geometry -------------------
+process.trackerGeometryDB.applyAlignment = cms.bool(False)
+process.XMLFromDBSource.label=''
+process.PoolDBESSourceGeometry = cms.ESSource("PoolDBESSource",
+        process.CondDBSetup,
+        timetype = cms.string('runnumber'),
+        toGet = cms.VPSet(
+                cms.PSet(
+                        record = cms.string('GeometryFileRcd'),
+                        tag = cms.string('XMLFILE_Geometry_74YV2_Extended2015_mc')
+                ),
+                cms.PSet(
+                        record = cms.string('IdealGeometryRecord'),
+                        tag = cms.string('TKRECO_Geometry_74YV2')
+                ),
+                cms.PSet(
+                        record = cms.string('PGeometricDetExtraRcd'),
+                        tag = cms.string('TKExtra_Geometry_74YV2')
+                ),
+                cms.PSet(
+                        record = cms.string('PTrackerParametersRcd'),
+                        tag = cms.string('TKParameters_Geometry_74YV2')
+                ),
+                cms.PSet(
+                        record = cms.string('PEcalBarrelRcd'),
+                        tag = cms.string('EBRECO_Geometry_74YV2')
+                ),
+                cms.PSet(
+                        record = cms.string('PEcalEndcapRcd'),
+                        tag = cms.string('EERECO_Geometry_74YV2')
+                ),
+                cms.PSet(
+                        record = cms.string('PEcalPreshowerRcd'),
+                        tag = cms.string('EPRECO_Geometry_74YV2')
+                ),
+                cms.PSet(
+                        record = cms.string('PHcalRcd'),
+                        tag = cms.string('HCALRECO_Geometry_74YV2')
+                ),
+                cms.PSet(
+                        record = cms.string('PCaloTowerRcd'),
+                        tag = cms.string('CTRECO_Geometry_74YV2')
+                ),
+                cms.PSet(
+                        record = cms.string('PZdcRcd'),
+                        tag = cms.string('ZDCRECO_Geometry_74YV2')
+                ),
+                cms.PSet(
+                        record = cms.string('PCastorRcd'),
+                        tag = cms.string('CASTORRECO_Geometry_74YV2')
+                ),
+                cms.PSet(
+                        record = cms.string('CSCRecoGeometryRcd'),
+                        tag = cms.string('CSCRECO_Geometry_74YV2')
+                ),
+                cms.PSet(
+                        record = cms.string('CSCRecoDigiParametersRcd'),
+                        tag = cms.string('CSCRECODIGI_Geometry_74YV2')
+                ),
+                cms.PSet(
+                        record = cms.string('DTRecoGeometryRcd'),
+                        tag = cms.string('DTRECO_Geometry_74YV2')
+                ),
+                cms.PSet(
+                        record = cms.string('RPCRecoGeometryRcd'),
+                        tag = cms.string('RPCRECO_Geometry_74YV2')
+                )
+        ),
+        connect = cms.string('sqlite_file:./PilotGeometry.db') 
+)
+process.es_prefer_geometry = cms.ESPrefer( "PoolDBESSource", "PoolDBESSourceGeometry" )
+
 
 
 process.source = cms.Source("EmptyIOVSource",
@@ -29,15 +104,7 @@ process.PoolDBOutputService = cms.Service("PoolDBOutputService",
     loadBlobStreamer = cms.untracked.bool(False)
 )
 
-#process.MessageLogger = cms.Service("MessageLogger",
-#    debugModules = cms.untracked.vstring('*'),
-#    destinations = cms.untracked.vstring('out'),
-#    out = cms.untracked.PSet( threshold = cms.untracked.string('DEBUG'))
-#)
-
 process.load("CalibTracker.SiPixelConnectivity.PixelToLNKAssociateFromAsciiESProducer_cfi")
-
-
 process.pixelToLNKAssociateFromAscii.fileName = cms.string('pixelToLNK_pilot.ascii')
 
 process.mapwriter = cms.EDAnalyzer("SiPixelFedCablingMapWriter",

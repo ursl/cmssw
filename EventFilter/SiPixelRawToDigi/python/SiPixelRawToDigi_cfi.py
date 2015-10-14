924a503295df088f2297987c8d6bcb37de909c63
Urs Langenegger <urs.langenegger@psi.ch>
2015-10-14 17:34:39 +0200

    turn on pilot blade

--- EventFilter/SiPixelRawToDigi/python/SiPixelRawToDigi_cfi.py
+++ EventFilter/SiPixelRawToDigi/python/SiPixelRawToDigi_cfi.py
@@ -1,18 +1,20 @@
 import FWCore.ParameterSet.Config as cms
-import EventFilter.SiPixelRawToDigi.siPixelRawToDigi_cfi
 
-siPixelDigis = EventFilter.SiPixelRawToDigi.siPixelRawToDigi_cfi.siPixelRawToDigi.clone()
-siPixelDigis.Timing = cms.untracked.bool(False)
-siPixelDigis.IncludeErrors = cms.bool(True)
-siPixelDigis.InputLabel = cms.InputTag("siPixelRawData")
-siPixelDigis.UseQualityInfo = cms.bool(False)
+siPixelDigis = cms.EDProducer("SiPixelRawToDigi",
+    Timing = cms.untracked.bool(False),
+    IncludeErrors = cms.bool(True),
+    InputLabel = cms.InputTag("siPixelRawData"),
+    CheckPixelOrder = cms.bool(False),
+    UseQualityInfo = cms.bool(False),
 ## ErrorList: list of error codes used by tracking to invalidate modules
-siPixelDigis.ErrorList = cms.vint32(29)
+    ErrorList = cms.vint32(29),
 ## UserErrorList: list of error codes used by Pixel experts for investigation
-siPixelDigis.UserErrorList = cms.vint32(40)
+    UserErrorList = cms.vint32(40),
 ##  Use pilot blades
-siPixelDigis.UsePilotBlade = cms.bool(False)
+    UsePilotBlade = cms.bool(True),
 ##  Use phase1
-siPixelDigis.UsePhase1 = cms.bool(False)
+    UsePhase1 = cms.bool(False),
 ## Empty Regions PSet means complete unpacking
-siPixelDigis.Regions = cms.PSet( ) 
+    Regions = cms.PSet( ) 
+)
+
